dcfca9f1931e292d97e1a9abea056ecff48835df
Tanishka2626 <Tanishkajain733@gmail.com>
2025-08-31 19:13:55 +0530

    Add area of rectangle

--- hello.c
+++ hello.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello, World!\n");
-    return 0;
-}
+    // int length =3;
+    // int breadth =6;
+    int length , breadth;
+    printf(" Enter length\n");
+    scanf("%d", &length);
+
+    printf("Enter breadth\n");
+    scanf("%d", &breadth);
+
+    printf("The area of rectangle is %d", length*breadth);
+   return 0;
+ }
+
+
