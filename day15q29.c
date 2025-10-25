7bdb9e410efbaede454df29681cb628faaccf0a3
Tanishka2626 <Tanishkajain733@gmail.com>
2025-10-25 12:01:29 +0530

    factorial of a number

---
+++ day15q29.c
@@ -0,0 +1,9 @@
+//Write a program to calculate the factorial of a number.
+#include <stdio.h>
+int main(){
+    int n, fact = 1;
+    scanf("%d", &n);
+    for(int i = 1; i <=n; i++)
+    fact *= i;
+    printf("%d", fact);
+}
