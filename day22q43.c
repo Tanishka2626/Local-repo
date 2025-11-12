3b14939548f5f44e86e97a32d486b124823fb32a
Tanishka2626 <Tanishkajain733@gmail.com>
2025-11-12 19:33:25 +0530

    k3bq43

---
+++ day22q43.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int num, temp, digit, sum = 0, fact, i;
+
+    // Input number from user
+    printf("Enter a number to check if it's a Strong number: ");
+    scanf("%d", &num);
+
+    temp = num;  // Store original number
+
+    // Loop to extract each digit and calculate factorial
+    while (temp > 0) {
+        digit = temp % 10;   // Get last digit
+        fact = 1;
+
+        // Find factorial of the digit
+        for (i = 1; i <= digit; i++) {
+            fact *= i;
+        }
+
+        sum += fact;          // Add factorial to sum
+        temp /= 10;           // Remove last digit
+    }
+
+    // Check if number equals sum of factorials of its digits
+    if (sum == num)
+        printf("%d is a Strong number.\n", num);
+    else
+        printf("%d is not a Strong number.\n", num);
+
+    return 0;
+}
