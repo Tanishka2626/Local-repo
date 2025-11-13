bb33ae0513ffba4d0b0d72913b65714e9985da18
Tanishka2626 <Tanishkajain733@gmail.com>
2025-11-13 07:17:34 +0530

    k3bq51

---
+++ day26q51.c
@@ -0,0 +1,21 @@
+/* Write  program to print the following pattern:
+    5
+   45
+  345
+ 2345
+12345
+*/
+#include <stdio.h>
+int main() {
+    int a, b, c;
+    for(a = 5; a >= 1; a--) {
+        for(b = 1; b < a; b++) {
+            printf(" ");
+        }
+        for(c = a; c <= 5; c++) {
+            printf("%d", c);
+        }
+        printf("\n");
+    }
+    return 0;
+}
