d3dd9faf7bfcd32e313ff7d2868ddea71355a0a6
Tanishka2626 <Tanishkajain733@gmail.com>
2025-11-13 08:16:00 +0530

    k3bq65

---
+++ day33q65.cc
@@ -0,0 +1,29 @@
+// Search in a sorted array using binary search.
+#include <stdio.h>
+int main() {
+    int arr[30], n, key, low = 0, high, mid;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    high = n - 1;
+    while(low <= high) {
+        mid = (low + high) / 2;
+        if(arr[mid] == key) {
+            printf("Found at position %d", mid + 1);
+            return 0;
+        }
+        else if(arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    printf("Not found");
+    return 0;
+}
